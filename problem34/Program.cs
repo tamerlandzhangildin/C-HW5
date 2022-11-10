e61da40d2c18f9285ed7daaa938c875f5205ace7
Tamerlan <dzhangtim@list.ru>
2022-11-10 18:51:46 +0300

    newfile

---
+++ problem34/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[size];
+            int k = 0;
+
+            Random rnd = new Random();
+ 
+            Console.WriteLine("Исходный массив");
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = rnd.Next(100,1000);
+                Console.Write("{0} ", arr[i]);
+            }
+            Console.WriteLine();
+        
+        
+            foreach(int element in arr)
+            {
+                Console.Write("{0} ", element);
+                if (element % 2 == 0)
+                {
+                    k++;
+                    
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Количество четных элементов {0}", k);
